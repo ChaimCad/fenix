14a9d00ad21599b1a17b2a8154cfcc96078ebed9
deborabertoluci <debertoluci@gmail.com>
2019-05-03 17:10:58 -0300

    pagina area de atuacao ligada qualificacoes

--- src/pages/pags_curriculo/area-atuacao/area-atuacao.ts
+++ src/pages/pags_curriculo/area-atuacao/area-atuacao.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { directive } from '@angular/core/src/render3/instructions';
+import { AlertController } from 'ionic-angular';
+import { QualificacoesPage } from '../qualificacoes/qualificacoes';
 
 /**
  * Generated class for the AreaAtuacaoPage page.
@@ -15,13 +18,40 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class AreaAtuacaoPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public cont = 0;
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad AreaAtuacaoPage');
   }
 
-  
+  cria(){
+   this.cont++;
+   //console.log(this.cont)
+   if (this.cont==1){
+     document.getElementById('sec1').style.display='unset';
+   }
+   if (this.cont==2){
+    document.getElementById('sec2').style.display='unset';
+  }
+
+  if (this.cont > 2){
+    const alert = this.alertCtrl.create({
+      title: 'Erro!',
+      subTitle: 'Só é possível inserir três áreas de atuação e três cursos.',
+      buttons: ['OK']
+    });
+    alert.present();
+    
+  }
+    
+  }
+
+  goToPage5(){
+    
+    this.navCtrl.push(QualificacoesPage);
+  }
 
 }
