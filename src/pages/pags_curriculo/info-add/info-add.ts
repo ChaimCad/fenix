bb1b4ee6e9786c4f5cb25a089bcc99c8b753e4ef
Giovanna Moeller <giovannamoeller18@gmail.com>
2019-05-07 16:31:12 -0300

    alteranco css e info adicionados

--- src/pages/pags_curriculo/info-add/info-add.ts
+++ src/pages/pags_curriculo/info-add/info-add.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
 
 /**
  * Generated class for the InfoAddPage page.
@@ -15,11 +16,35 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class InfoAddPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl:AlertController) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InfoAddPage');
+  }
+
+  public formulario_infoadd:FormGroup = new FormGroup({
+    'experiencia' :  new FormControl(null, [Validators.required, Validators.minLength(10), Validators.maxLength(300)])
+  })
+
+  public salvar(){
+    if (this.formulario_infoadd.status != "INVALID") {
+      let alert1 = this.alertCtrl.create({
+        title: 'Sucesso!',
+        subTitle: 'Seu currículo foi realizado com sucesso.',
+        buttons: ['Ok']
+      });
+      alert1.present();
+      
+    }
+    else {
+      let alert = this.alertCtrl.create({
+        title: 'Atenção!',
+        subTitle: 'Preencha no mínimo 10 e no máximo 300 caracteres.',
+        buttons: ['Ok']
+      });
+      alert.present();
+    }
   }
 
 }
