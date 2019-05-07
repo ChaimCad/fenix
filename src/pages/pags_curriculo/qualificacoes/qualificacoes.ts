a8ad175c5229623be0f2ba37fb16e942999a1bf0
Maria Eduarda Bello Martins <mariaeduardabellomartins@hotmail.com>
2019-05-07 15:23:29 -0300

    alteracoes qualificacoes

--- src/pages/pags_curriculo/qualificacoes/qualificacoes.ts
+++ src/pages/pags_curriculo/qualificacoes/qualificacoes.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { InfoAddPage } from '../info-add/info-add';
 
 /**
  * Generated class for the QualificacoesPage page.
@@ -15,36 +16,33 @@ import { FormControl, FormGroup, Validators } from '@angular/forms';
   templateUrl: 'qualificacoes.html',
 })
 export class QualificacoesPage {
-  alertCtrl: any;
+  //alertCtrl: any;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
   }
 
   ano: number;
-  mes: number;
+
 
   public RetornaAno() {
     var dNow = new Date();
-
-    this.mes = dNow.getMonth()
-    //return mes
 
     this.ano = dNow.getFullYear()
     //console.log(this.ano) 
   }
 
   public formulario: FormGroup = new FormGroup({
-    'curso': new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(20), Validators.pattern('[A-Za-zÀ-ú]*')]),
-    'nome_inst': new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(30), Validators.pattern('[A-Za-zÀ-ú]*')]),
+    'curso': new FormControl(null, [Validators.required, Validators.pattern('[A-Za-zÀ-ú]*')]),
+    'nome_inst': new FormControl(null, [Validators.required, Validators.minLength(4), Validators.maxLength(50), Validators.pattern('[ A-Za-zÀ-ú ]*')]),
     'ano': new FormControl(null, [Validators.required]),
-    'tipo_curs': new FormControl(null, [Validators.required]),
-    'desc': new FormControl(null, [Validators.required, Validators.maxLength(10), Validators.pattern('[A-Za-zÀ-ú]*')]),
+    'tipo_curso': new FormControl(null, [Validators.required]),
+    'desc': new FormControl(null, [Validators.required, Validators.pattern('[A-Za-zÀ-ú]*')]),
   })
 
-  public tipo_curs: Array<string> = [
+  public tipo_curso: Array<string> = [
+    'Técnico',
     'Graduação',
     'Pós-graduação',
-    'Técnico',
     'Mestrado',
     'Doutorado',
     'Outros'
@@ -63,16 +61,17 @@ export class QualificacoesPage {
       else if (!this.formulario.controls.nome_inst.valid || this.formulario.controls.nome_inst.touched)
         document.getElementById('lbl_' + name).style.color = '#f53d3d';
     }
-    if (name == 'tipo_curs') {
-      if (this.formulario.controls.tipo_curs.valid)
-        document.getElementById('lbl_' + name).style.color = '#32db64';
-      else if (!this.formulario.controls.tipo_curs.valid || this.formulario.controls.tipo_curs.touched)
-        document.getElementById('lbl_' + name).style.color = '#f53d3d';
-    }
+
     if (name == 'ano') {
       if (this.formulario.controls.ano.valid)
         document.getElementById('lbl_' + name).style.color = '#32db64';
       else if (!this.formulario.controls.ano.valid || this.formulario.controls.ano.touched)
+        document.getElementById('lbl_' + name).style.color = '#f53d3d';
+    }
+    if (name == 'tipo_curso') {
+      if (this.formulario.controls.tipo_curso.valid)
+        document.getElementById('lbl_' + name).style.color = '#32db64';
+      else if (!this.formulario.controls.tipo_curso.valid || this.formulario.controls.tipo_curso.touched)
         document.getElementById('lbl_' + name).style.color = '#f53d3d';
     }
     if (name == 'desc') {
@@ -84,26 +83,21 @@ export class QualificacoesPage {
   }
 
 
-  goToPage5() {
-    if (this.formulario.status != "INVALID") {
-
-      this.navCtrl.push(QualificacoesPage);
-    }
-    else {
-      let alert = this.alertCtrl.create({
-        title: 'Atenção!',
-        subTitle: 'Preencha todos os campos corretamente. Todos os campos são obrigatórios!',
-        buttons: ['Ok']
-      });
-      alert.present();
-    }
-
-
-  }
-
-
   ionViewDidLoad() {
     console.log('ionViewDidLoad QualificacoesPage');
   }
 
+  goToPage6() {
+    if (this.formulario.status != "INVALID") {
+      this.navCtrl.push(InfoAddPage);
+    }
+    else {
+      let alert = this.alertCtrl.create({
+        title: 'Atenção!',
+        subTitle: 'Preencha todos os campos corretamente.',
+        buttons: ['Ok']
+      });
+      alert.present();
+    }
+  }
 }
