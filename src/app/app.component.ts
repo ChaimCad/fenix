691a503a14e5fff64ce9dd340a7190b396ab1124
Giovanna Moeller <giovannamoeller18@gmail.com>
2019-05-09 21:52:16 -0300

    sera se vai

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,7 +6,6 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { TabsPage } from '../pages/tabs/tabs';
 import { HomePage } from '../pages/home/home';
 import { HotsitePage } from '../pages/hotsite/hotsite';
-import { CurriculoPage } from '../pages/curriculo/curriculo';
 import { Curriculo1Page } from '../pages/pags_curriculo/curriculo1/curriculo1';
 import { AboutPage } from '../pages/about/about';
 import { DadosPessoaisPage } from '../pages/pags_curriculo/dados-pessoais/dados-pessoais';
@@ -22,7 +21,7 @@ import { InfoAddPage } from '../pages/pags_curriculo/info-add/info-add';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = FormacaoPage;
+  rootPage:any = AreaAtuacaoPage;
 
   
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
