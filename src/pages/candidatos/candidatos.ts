f4ae42acf2cc06fff8cfcd641fd936da62d25e2f
Andre Moco Chaim <ra1757066@CTIBAURU.FEB.UNESP.BR>
2019-05-21 09:28:52 -0300

    div filtros com botão de animação

--- src/pages/candidatos/candidatos.ts
+++ src/pages/candidatos/candidatos.ts
@@ -20,7 +20,23 @@ export class CandidatosPage {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad CandidatosPage');
+    var x: number = 0;
+    document.getElementById('botaofiltro').addEventListener('click', function() {
+      if(x == 0) {
+        document.getElementById("filter").style.display = "flex";
+        for (let op = 0; op <= 1; op += 0.1) {
+            setTimeout(function() {
+              document.getElementById("filter").style.opacity = String(op);
+            }, 100);
+        }
+        x = 1;
+      }
+      else {
+        document.getElementById("filter").style.display = "none";
+        document.getElementById("filter").style.opacity = "0";
+        x = 0;
+      }
+    });
   }
 
   public sexo: Array<string> = [
@@ -42,23 +58,26 @@ export class CandidatosPage {
   public areaA: Array<string> = [
     'Informática',
     'Administração',
-    'Recursos humanos',
+    'Recursos humanos'
   ];
 
   public areaI: Array<string> = [
     'Informática',
     'Administração',
-    'Recursos humanos',
+    'Recursos humanos'
   ];
   
   public formulario_candidatos: FormGroup = new FormGroup({
-    'idade' : new FormControl(null, [Validators.max(90), Validators.pattern('[0-9]')])
+    'idade' : new FormControl(null, [Validators.max(90)])
   });
 
-  public Valida(name:string) {
+  public Valida(name: string) {
     if (name == 'idade') {
       if (this.formulario_candidatos.controls.idade.valid)
         document.getElementById('lbl_'+ name).style.color='#32db64';
+      else if(this.formulario_candidatos.controls.idade.value == null) {
+        document.getElementById('lbl_'+ name).style.color='#999';
+      }
       else if (!this.formulario_candidatos.controls.idade.valid || this.formulario_candidatos.controls.idade.touched) {
         document.getElementById('lbl_'+ name).style.color='#f53d3d';
       }
